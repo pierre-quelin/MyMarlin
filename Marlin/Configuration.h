c0f5b95b31e5000221e5fed769259209d95fe230
Pierre Quélin <pierre.quelin.1972@gmail.com>
2020-06-05 22:55:07 +0200

    Upgraded with aluminum dual gear pulley dual drive extruder kit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -614,7 +614,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 144 } // E0=95 Origin, E0=144 Upgraded with aluminum dual gear pulley dual drive extruder kit
 
 /**
  * Default Max Feed Rate (mm/s)
